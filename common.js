503e47644f8f7c0a035875a01f4fe6eef1f3b6ad
Koonkit <kanghs112211@gmail.com>
2024-02-09 17:19:17 +0700

    refactor#3 - modify return msg

--- common.js
+++ common.js
@@ -25,10 +25,10 @@ function calculate(p,d){
 		var dist = parseInt(d);
 	//---------------------------
 		var chk = validate(pace);
-		if (chk != null) return chk;
+		if(chk != null) return chk;
 	//---------------------------
-		var pc = paceCal(pace.dist);
+		var pc = paceCal(pace,dist);
 	//---------------------------
 		msg = "successfully calculated!"
-		return {"status":1,"message":msg,"hour":hour,"min":min};
+		return {"status":1,"message":msg,"hour":pc.hour,"min":pc.min};
 }
